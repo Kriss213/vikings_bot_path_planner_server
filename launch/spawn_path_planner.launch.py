03561d61142311d558d105d1ba222162af2124f5
Kriss213 <kriss.vasermanslv@gmail.com>
2025-04-08 13:19:01 +0300

    partial rewrite

--- launch/spawn_path_planner.launch.py
+++ launch/spawn_path_planner.launch.py
@@ -1,81 +1,44 @@
-import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node, SetParameter
-from launch.conditions import IfCondition, UnlessCondition
-from launch.actions import DeclareLaunchArgument, GroupAction
+from launch.actions import DeclareLaunchArgument, OpaqueFunction, GroupAction
 from launch.substitutions import (
     LaunchConfiguration,
     PathJoinSubstitution,
-    PythonExpression,
     PathJoinSubstitution
 )
 
+def launch_setup(context, *args, **kwargs):
 
-def generate_launch_description():
-
-    ### DATA INPUT ###
-    vikings_bot_name_arg = DeclareLaunchArgument("vikings_bot_name",
-        default_value="vikings_bot_1",
-        description="Namespace of robot - [vikings_bot_1 or vikings_bot_2]"
-    )
-
-    use_sim_arg = DeclareLaunchArgument("use_sim",
-        default_value="True",
-        description='Use simulation or real time'
-    )
-
-    use_lidar_arg = DeclareLaunchArgument(
-        'use_lidar',
-        default_value='True',
-        description="Use lidar for navigation"
-    )
-    
-    use_depth_cam_arg = DeclareLaunchArgument(
-        'use_depth_cam',
-        default_value='False',
-        description="Use depth camera for navigation"
-    )
-
-
-    vikings_bot_name = LaunchConfiguration("vikings_bot_name")
+    vikings_bot_name = LaunchConfiguration("namespace").perform(context)
     use_sim = LaunchConfiguration("use_sim")
-    use_lidar = LaunchConfiguration('use_lidar')
-    use_depth_cam = LaunchConfiguration('use_depth_cam')
 
     package_name = 'vikings_bot_path_planner_server'
 
-
     ### CONFIG FILES ###
-    controller_yaml = PathJoinSubstitution([get_package_share_directory(package_name), 'config', PythonExpression(["'", vikings_bot_name, "_controller.yaml'"]) ])
-    bt_navigator_yaml = PathJoinSubstitution([get_package_share_directory(package_name), 'config', PythonExpression(["'", vikings_bot_name, "_bt_navigator.yaml'"]) ])
-    planner_yaml = PathJoinSubstitution([get_package_share_directory(package_name), 'config', PythonExpression(["'", vikings_bot_name, "_planner_server.yaml'"]) ])
-    recovery_yaml = PathJoinSubstitution([get_package_share_directory(package_name), 'config', PythonExpression(["'", vikings_bot_name, "_recovery.yaml'"]) ])
-
+    controller_yaml = PathJoinSubstitution([get_package_share_directory(package_name), 'config', 'vikings_bot_controller.yaml'])
+    bt_navigator_yaml = PathJoinSubstitution([get_package_share_directory(package_name), 'config', 'vikings_bot_bt_navigator.yaml' ])
+    planner_yaml = PathJoinSubstitution([get_package_share_directory(package_name), 'config', 'vikings_bot_planner_server.yaml'])
+    recovery_yaml = PathJoinSubstitution([get_package_share_directory(package_name), 'config', 'vikings_bot_recovery.yaml'])
     behavior = PathJoinSubstitution([get_package_share_directory(package_name), 'config', 'behavior.xml'])
 
-    # topic and observation source change conditions for local_costmap controller
-    do_use_lidar = PythonExpression(["'", use_lidar, "'.lower() == 'true'"])
-    do_use_depth_cam = PythonExpression(["'", use_depth_cam, "'.lower() == 'true'"])
-    do_use_lidar_and_depth_cam = PythonExpression(["('", use_depth_cam, "'.lower() == 'true') and ('", use_lidar, "'.lower() == 'true')"])
    
     ### NODES ###
+    
     controller_node_n_params = GroupAction( # group to only set these params for this node
         actions=[
-            SetParameter( # set obesrvation sources to only lidar
-                name="voxel_layer.observation_sources",
-                value='scan',
-                condition=IfCondition(do_use_lidar)
+            SetParameter(
+                name="global_frame",
+                value=f"{vikings_bot_name}/odom" # add namespace to frame param
             ),
-            SetParameter( # set observation sources to only depth cam
-                name="voxel_layer.observation_sources",
-                value='point_cloud',
-                condition=IfCondition(do_use_depth_cam)
+            SetParameter(
+                name="robot_base_frame",
+                value=f'{vikings_bot_name}/base_link' # add namespace to frame param,
             ),
-            SetParameter( # set obesrvation sources to both lidar and depth cam
-                name="voxel_layer.observation_sources",
-                value='scan point_cloud',
-                condition=IfCondition(do_use_lidar_and_depth_cam)
+            SetParameter(
+                name="topic", # for scan observation source
+                value=f"/{vikings_bot_name}/lidar_scan"
+                # for some reason this is not set from namespace (maybe because it is a nested node in controller manager?)
             ),
             Node(
                 namespace=vikings_bot_name,
@@ -91,23 +54,17 @@ def generate_launch_description():
             )
         ]
     )   
-
+    
     planner_node_n_params = GroupAction( # group to only set these params for this node
         actions=[
-            SetParameter( # set obesrvation sources to only lidar
-                name="obstacle_layer.observation_sources",
-                value='scan',
-                condition=IfCondition(do_use_lidar)
+            SetParameter(
+                name="robot_base_frame",
+                value=f"{vikings_bot_name}/base_link" # add namespace to frame param
             ),
-            SetParameter( # set observation sources to only depth cam
-                name="obstacle_layer.observation_sources",
-                value='point_cloud',
-                condition=IfCondition(do_use_depth_cam)
-            ),
-            SetParameter( # set obesrvation sources to both lidar and depth cam
-                name="obstacle_layer.observation_sources",
-                value='scan point_cloud',
-                condition=IfCondition(do_use_lidar_and_depth_cam)
+            SetParameter(
+                name="topic", # for scan observation source
+                value=f"/{vikings_bot_name}/lidar_scan"
+                # for some reason this is not set from namespace (maybe because it is a nested node in planner server?)
             ),
             Node(
                 namespace=vikings_bot_name,
@@ -118,7 +75,8 @@ def generate_launch_description():
                 respawn=True,
                 parameters=[
                     planner_yaml,
-                    {'use_sim_time': use_sim}
+                    {'use_sim_time': use_sim},
+                    #{'robot_base_frame': f'{vikings_bot_name}/base_link'} # add namespace to frame param
                 ]
             )
         ]
@@ -130,11 +88,12 @@ def generate_launch_description():
         executable='behavior_server',
         name='behavior_server',
         respawn=True,
+        output='screen',
         parameters=[
             recovery_yaml,
-            {'use_sim_time': use_sim}
+            {'use_sim_time': use_sim},
+            {'robot_base_frame': f'{vikings_bot_name}/base_link'} # add namespace to frame param
         ],
-        output='screen',
     )
 
     navigator = Node(
@@ -147,6 +106,7 @@ def generate_launch_description():
         parameters=[
             bt_navigator_yaml,
             {"default_nav_to_pose_bt_xml": behavior},
+            {'robot_base_frame': f'{vikings_bot_name}/base_link'}, # add namespace to frame param
             {'use_sim_time': use_sim}
         ]
     )
@@ -170,19 +130,40 @@ def generate_launch_description():
         ]
     )
 
-    return LaunchDescription([
-        vikings_bot_name_arg,
-        use_sim_arg,
-        use_lidar_arg,
-        use_depth_cam_arg,
-
-        SetParameter('use_sim_time',  use_sim),
-
-        controller_node_n_params,
+    return [controller_node_n_params,
         planner_node_n_params,
         behaviour,
         navigator,
-        lifecycle
+        lifecycle]# + local_costmap_params + global_costmap_params
+
+def generate_launch_description():
+
+    ### DATA INPUT ###
+    namespace_arg = DeclareLaunchArgument("namespace",
+        default_value="vikings_bot_1",
+        description="Namespace of robot"
+    )
+
+    use_sim_arg = DeclareLaunchArgument("use_sim",
+        default_value="True",
+        description='Use simulation or real time'
+    )
+
+    # vikings_bot_name = LaunchConfiguration("namespace")
+    use_sim = LaunchConfiguration("use_sim")
+
+    # package_name = 'vikings_bot_path_planner_server'
+
+
+    
+
+    return LaunchDescription([
+        namespace_arg,
+        use_sim_arg,
+        
+        SetParameter('use_sim_time',  use_sim),
+
+        OpaqueFunction(function=launch_setup)
 
     ])
 
